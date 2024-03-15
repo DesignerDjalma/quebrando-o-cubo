57d08aa4c561f8320ea019d500ec55855f9b738d
Djalma Filho <designer.djalma@gmail.com>
2024-03-15 13:57:27 -0300

    main2 att

--- main2.py
+++ main2.py
@@ -1,26 +1,22 @@
-from ursina import *
+from ursina import Ursina, Entity, Text # type: ignore
+from ursina import invoke, held_keys, color, time
 
 # create a window
-app = Ursina()
+app = Ursina(
+    title="Ursina Window",
+)
 
-# most things in ursina are Entities. An Entity is a thing you place in the world.
-# you can think of them as GameObjects in Unity or Actors in Unreal.
-# the first parameter tells us the Entity's model will be a 3d-model called 'cube'.
-# ursina includes some basic models like 'cube', 'sphere' and 'quad'.
 
-# the next parameter tells us the model's color should be orange.
-
-# 'scale_y=2' tells us how big the entity should be in the vertical axis, how tall it should be.
-# in ursina, positive x is right, positive y is up, and positive z is forward.
-
-player = Entity(model='cube', color=color.orange, scale_y=2)
-
+jogador: Entity = Entity(model='cube', color=color.orange, scale_y=1, scale_x=1)
+score: Text = Text(text="Vida")
 # create a function called 'update'.
 # this will automatically get called by the engine every frame.
 
 def update():
-    player.x += held_keys['d'] * time.dt
-    player.x -= held_keys['a'] * time.dt
+    jogador.x += held_keys['d'] * time.dt
+    jogador.x -= held_keys['a'] * time.dt
+    jogador.y += held_keys['w'] * time.dt
+    jogador.y -= held_keys['s'] * time.dt
 
 # this part will make the player move left or right based on our input.
 # to check which keys are held down, we can check the held_keys dictionary.
@@ -31,8 +27,8 @@ def update():
 
 def input(key):
     if key == 'space':
-        player.y += 1
-        invoke(setattr, player, 'y', player.y-1, delay=.25)
+        jogador.y += 1
+        invoke(setattr, jogador, 'y', jogador.y-1, delay=.25)
 
 
 # start running the game
