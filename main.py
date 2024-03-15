7d3da2e72c681edff32b6444f8040aa675e2fcf7
Maurício Henrique <mauri.jaw@outlook.com>
2024-03-15 14:10:54 -0300

    texture

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from ursina import *
 app = Ursina()
 speed = 10
 
-player = Entity(model='circle', color=color.orange, scale_y=2)
+player = Entity(model='cube', color=color.orange, scale_y=2)
+box = Entity(model='cube', color=color.white,scale_x=1)
+
+
+box.collider = 'mesh'
+player.collider = 'mesh'
+
 def update():
     player.x += (held_keys['d'] - held_keys['a']) * time.dt * speed
-    player.y += (held_keys['w'] - held_keys['s']) * time.dt * speed
+    player.z += (held_keys['w'] - held_keys['s']) * time.dt * speed
+
+    box.z += (held_keys['i'] - held_keys['k']) * time.dt * speed 
     pass
 
 app.run()
